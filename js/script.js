4c51a9d86228c98f826d268e952a04729284bafa
ViniciusCelio <viniciusceliofontes@gmail.com>
2023-05-24 20:39:07 -0300

    Adicionando menu 'sanduiche' e botão detalhes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myFunction () {
+    const x = document.querySelector(".X");
+    const menu = document.querySelector(".menu");
+    
+    x.classList.toggle("sanduiche");
+    menu.classList.toggle("escondeMenu");
+  }
+  
