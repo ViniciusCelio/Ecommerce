0af7e9eb58c9ccb001448e4d87530c93b8d30276
ViniciusCelio <viniciusceliofontes@gmail.com>
2023-05-29 23:37:41 -0300

    Começando a criar o filtro na Home Page

--- js/script.js
+++ js/script.js
@@ -6,21 +6,44 @@ function myFunction () {
   menu.classList.toggle("escondeMenu");
 }
 
-
 fetch('https://fakestoreapi.com/products')
-      .then(resposta=>resposta.json())
-      .then(dados => {
-        let str = '';
-        for (let i = 0; i < dados.length; i++) {
+    .then(resposta=>resposta.json())
+    .then(dados => {
+      let str = '';
+      for (let i = 0; i < dados.length; i++) {
           str = str + `<div class="card-produtos">
-                    <img src="${dados[i].image}" alt="">
-                    <p class="nome">${dados[i].title}</p>
-                    <p class="preco">R$ ${dados[i].price.toFixed(2)}</p>
-                    <a href="#" class="detalhes">Detalhes</a>
-                    <p class="avaliacao">Avaliação: ${dados[i].rating.rate} (<span style="color:#6f6f6f">${dados[i].rating.count}</span>)</p>
-                </div>`;
-        }
-        
-        document.querySelector('.produtos').innerHTML = str;
-      });
+                  <img src="${dados[i].image}" alt="">
+                  <p class="nome">${dados[i].title}</p>
+                  <p class="preco">R$ ${dados[i].price.toFixed(2)}</p>
+                  <a href="#" class="detalhes">Detalhes</a>
+                  <p class="avaliacao">Avaliação: ${dados[i].rating.rate} (<span style="color:#6f6f6f">${dados[i].rating.count}</span>)</p>
+                  <label style="display:none">${dados[i].category}</label>
+              </div>`;
+      }
+
+      document.querySelector('.produtos').innerHTML = str;
+      
+    });
+
+const select = document.querySelector("#filtro");
+
+select.addEventListener('change', FiltraProduto);
+
+function FiltraProduto () {
+  const categorias = document.querySelectorAll('.produtos .card-produtos');
+  var indice = select.selectedIndex;
+  var valorSelecionado = select.value;
+
+  for(let card of categorias) {
+    let categoria = card.querySelector('label').innerHTML;
+    /*if (categoria == all) {
+      card.style.display = "block";
+    }*/
+    
+    if (categoria != valorSelecionado) {
+      card.style.display = "none";
+    }
+  }
+
+}
 
