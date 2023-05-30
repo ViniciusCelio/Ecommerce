69841125f69c34d5c887ecb258e6cd86693a1f19
ViniciusCelio <viniciusceliofontes@gmail.com>
2023-05-29 22:00:22 -0300

    Consumindo dados da API

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 function myFunction () {
-    const x = document.querySelector(".X");
-    const menu = document.querySelector(".menu");
+  const x = document.querySelector(".X");
+  const menu = document.querySelector(".menu");
     
-    x.classList.toggle("sanduiche");
-    menu.classList.toggle("escondeMenu");
-  }
-  
+  x.classList.toggle("sanduiche");
+  menu.classList.toggle("escondeMenu");
+}
+
+
+fetch('https://fakestoreapi.com/products')
+      .then(resposta=>resposta.json())
+      .then(dados => {
+        let str = '';
+        for (let i = 0; i < dados.length; i++) {
+          str = str + `<div class="card-produtos">
+                    <img src="${dados[i].image}" alt="">
+                    <p class="nome">${dados[i].title}</p>
+                    <p class="preco">R$ ${dados[i].price.toFixed(2)}</p>
+                    <a href="#" class="detalhes">Detalhes</a>
+                    <p class="avaliacao">Avaliação: ${dados[i].rating.rate} (<span style="color:#6f6f6f">${dados[i].rating.count}</span>)</p>
+                </div>`;
+        }
+        
+        document.querySelector('.produtos').innerHTML = str;
+      });
+
